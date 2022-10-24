55a5e487959748cbe93223ceb03f2222a784de9f
Fabian Seluska <fabianseluska@gmail.com>
2022-10-24 13:05:01 +0200

    Added water bar.

--- lib/cat.dart
+++ lib/cat.dart
@@ -6,15 +6,12 @@ class Cat {
   // This is a callback function that is run whenever the cat's state changes.
   late Function() _onStatChange;
 
+  // START: Meow
   // This is needed for the cat to know if it should meow -- how else would it know?
   bool _shouldMeow = false;
 
   // Create an audio player for use with meows. :3
   final AudioPlayer _player = AudioPlayer();
-
-  // This keeps track of the cat's hunger and maxHunger.
-  int _foodSupply = 5;
-  final int maximumFoodSupply = 10;
 
   // This is a list of all possible meow sounds.
   static const _meows = [
@@ -38,10 +35,30 @@ class Cat {
     }
   }
 
+  // The cat will start meowing until stopped.
+  void startMeow() {
+    _shouldMeow = true;
+    // Only start meowing if not already meowing.
+    if (_player.state != PlayerState.playing) {
+      _meow();
+    }
+  }
+
+  // This stops the cat from meowing, but it will allow the current meow to finish.
+  void stopMeow() {
+    _shouldMeow = false;
+  }
+  // END: Meow
+
+  // START: Food
+  // This keeps track of the cat's hunger and maxHunger.
+  int _foodSupply = 5;
+  final int maxFoodSupply = 10;
+
   // Increases the cat's hunger by 1.
   void _decreaseFoodSupply() {
-    if (_foodSupply < maximumFoodSupply && Random().nextInt(10) == 7) {
-      _foodSupply += 1;
+    if (_foodSupply > 0) {
+      _foodSupply -= 1;
       _onStatChange();
     }
   }
@@ -49,38 +66,108 @@ class Cat {
   // Start a periodic timer that increases hunger.
   void _startFoodConsumption() {
     Timer.periodic(const Duration(seconds: 1), (timer) {
-      _decreaseFoodSupply();
+      if (Random().nextInt(10) == 7) {
+        _decreaseFoodSupply();
+      }
 
-      if (_foodSupply > 9) {
+      if (_foodSupply == 0) {
         startMeow();
       }
     });
   }
 
-  // The cat will start meowing until stopped.
-  void startMeow() {
-    _shouldMeow = true;
-    _meow();
-  }
-
-  // This stops the cat from meowing, but it will allow the current meow to finish.
-  void stopMeow() {
-    _shouldMeow = false;
-  }
-
-  // Feed the cat; decreases hunger by 1.
+  // Feed the cat; increases food supply by 1.
   void feed() {
-    if (_foodSupply > 0) {
-      _foodSupply -= 1;
+    if (_foodSupply < maxFoodSupply) {
+      _foodSupply += 1;
+      _onStatChange();
     }
-    stopMeow();
-    _onStatChange();
   }
 
   // Let the outside world see the cat's hunger.
-  int getHunger() {
+  int get remainingFoodSupply {
     return _foodSupply;
   }
+  // END: Food
+
+  // START: Water
+  // Keep track of the water supply.
+  int _waterSupply = 5;
+  final int maxWaterSupply = 10;
+
+  // Decrease the water supply by 1.
+  void _decreaseWaterSupply() {
+    if (_waterSupply > 0) {
+      _waterSupply -= 1;
+      _onStatChange();
+    }
+  }
+
+  // Start a periodic timer that makes sure that the cat gets thirsty.
+  void _startThirst() {
+    Timer.periodic(const Duration(seconds: 2), (timer) {
+      if (Random().nextInt(10) == 7) {
+        _decreaseWaterSupply();
+      }
+
+      if (_waterSupply == 0) {
+        startMeow();
+      }
+    });
+  }
+
+  // Give the cat some water; increases the water supply by 1.
+  void water() {
+    if (_waterSupply < maxWaterSupply) {
+      _waterSupply += 1;
+      _onStatChange();
+    }
+  }
+
+  // Get the remaining amount of water.
+  int get remainingWaterSupply {
+    return _waterSupply;
+  }
+  // END: Water
+
+  // START: Happiness
+  // Keep track of the cat's happiness levels
+  int _happinessLevel = 10;
+  final int maxHappinessLevel = 20;
+
+  // Decrease happiness level by 1.
+  void _decreaseHappinessLevel() {
+    if (_happinessLevel > 0) {
+      _happinessLevel -= 1;
+      _onStatChange();
+    }
+  }
+
+  // The cat will periodically lose happiness.
+  void _startBoredom() {
+    Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      if (Random().nextInt(17) == 7) {
+        _decreaseHappinessLevel();
+      }
+
+      if (_happinessLevel == 0) {
+        startMeow();
+      }
+    });
+  }
+
+  // Increase happiness level by 1.
+  void pet() {
+    if (_happinessLevel < maxHappinessLevel) {
+      _happinessLevel += 1;
+      _onStatChange();
+    }
+  }
+
+  int get remainingHappiness {
+    return _happinessLevel;
+  }
+  // END: Happiness
 
   // Initialize the cat.
   Cat({required Function() onStatChange}) {
@@ -91,6 +178,12 @@ class Cat {
 
     // Make sure the cat gets hungrier.
     _startFoodConsumption();
+
+    // Make sure the cat becomes bored.
+    _startBoredom();
+
+    // Make sure the cat gets thirsty.
+    _startThirst();
 
     // Make sure to add a callback to be run on a stat change.
     _onStatChange = onStatChange;
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'cat.dart';
 
 String catPic = "assets/el_gatos.jpg";
 double hungerBarValue = 0.0;
+double happinessBarValue = 0.0;
+double waterBarValue = 0.0;
 
 void main() => runApp(const MyApp());
 
@@ -14,6 +16,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // Pad an element
+  Widget padElement(
+      {required Widget element,
+      double vertical = 0.0,
+      double horizontal = 0.0}) {
+    return Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+        child: element);
+  }
+
   // Change which cat pic is displayed.
   changeCatPic(String url) {
     setState(() {
@@ -22,22 +35,21 @@ class _MyAppState extends State<MyApp> {
   }
 
   // Change the hunger bar.
-  hungerBar() {
+  updateStatusBars() {
     setState(() {
-      hungerBarValue =
-          cat.maximumFoodSupply - cat.getHunger() / cat.maximumFoodSupply;
+      hungerBarValue = cat.remainingFoodSupply / cat.maxFoodSupply;
+      happinessBarValue = cat.remainingHappiness / cat.maxHappinessLevel;
+      waterBarValue = cat.remainingWaterSupply / cat.maxWaterSupply;
     });
   }
 
   // This is ugly, but some form of initialization is required...
-  Cat cat = Cat(onStatChange: () {});
+  late Cat cat;
 
   @override
   void initState() {
     // Actually initialize.
-    cat = Cat(onStatChange: () {
-      hungerBar();
-    });
+    cat = Cat(onStatChange: updateStatusBars);
     super.initState();
   }
 
@@ -73,49 +85,102 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               const Text("My Garden with my Annoying Kittens! <3 "),
-              Center(child: Image.asset(catPic)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Center(
+                  child: padElement(
+                      element: Image.asset(catPic), horizontal: 50.0)),
+              Wrap(
+                alignment: WrapAlignment.spaceEvenly,
+                direction: Axis.horizontal,
                 children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        changeCatPic("assets/el_gatos2.jpg");
-                        cat.startMeow();
-                      },
-                      child: const Text("Meow! ^>^")),
-                  ElevatedButton(
-                      onPressed: () {
-                        changeCatPic("assets/el_gatos.jpg");
-                        cat.stopMeow();
-                      },
-                      child: const Text("SHUT UP! >.<")),
-                  ElevatedButton(
-                      onPressed: () {
-                        cat.feed();
-                      },
-                      child: const Text("Feed! :3")),
+                  padElement(
+                      element: ElevatedButton(
+                          onPressed: () {
+                            changeCatPic("assets/el_gatos2.jpg");
+                            cat.startMeow();
+                          },
+                          child: const Text("Meow! ^>^")),
+                      horizontal: 20.0,
+                      vertical: 5.0),
+                  padElement(
+                      element: ElevatedButton(
+                          onPressed: () {
+                            changeCatPic("assets/el_gatos.jpg");
+                            cat.stopMeow();
+                          },
+                          child: const Text("Quiet down! >.<")),
+                      horizontal: 20.0,
+                      vertical: 5.0),
+                  padElement(
+                      element: ElevatedButton(
+                          onPressed: () {
+                            cat.feed();
+                          },
+                          child: const Text("Feed! :3")),
+                      horizontal: 20.0,
+                      vertical: 5.0),
+                  padElement(
+                      element: ElevatedButton(
+                          onPressed: () {
+                            cat.water();
+                          },
+                          child: const Text("Water! :0")),
+                      horizontal: 20.0,
+                      vertical: 5.0),
+                  padElement(
+                      element: ElevatedButton(
+                          onPressed: () {
+                            cat.pet();
+                          },
+                          child: const Text("Pet! <3")),
+                      horizontal: 20.0,
+                      vertical: 5.0),
                 ],
               ),
-              Column(
-                children: [
-                  const Text("Food:"),
-                  LinearProgressIndicator(
-                    minHeight: 10,
-                    value: hungerBarValue,
-                    backgroundColor: theme.backgroundColor,
+              padElement(
+                  element: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            child: const Text("Food supply:"),
+                          ),
+                          LinearProgressIndicator(
+                            minHeight: 10,
+                            value: hungerBarValue,
+                            backgroundColor: theme.backgroundColor,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            child: const Text("Water:"),
+                          ),
+                          LinearProgressIndicator(
+                            minHeight: 10,
+                            value: waterBarValue,
+                            backgroundColor: theme.backgroundColor,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            child: const Text("Happiness:"),
+                          ),
+                          LinearProgressIndicator(
+                            minHeight: 10,
+                            value: happinessBarValue,
+                            backgroundColor: theme.backgroundColor,
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text("Happiness:"),
-                  LinearProgressIndicator(
-                    minHeight: 10,
-                    value: 0.5,
-                    backgroundColor: theme.backgroundColor,
-                  ),
-                ],
-              )
+                  horizontal: 50.0)
             ],
           )),
     );
